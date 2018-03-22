e23d164c968d0d67fa690f79c86d45fac623e7c7
phelipe <phelipewesleydeoliveira@gmail.com>
2018-03-21 21:26:22 -0300

    correção de erro

--- src/search.jl
+++ src/search.jl
@@ -10,7 +10,6 @@ r2 = 0.6 # referência junta 2
 
 
 function custo(x::Vector{Float64})
-
     kp = SMatrix{2,2}(diagm([x[1], x[2]]))
     kv = SMatrix{2,2}(diagm([x[3], x[4]]))
     x, v, t, a, ta, j, tj = simulation(kp, kv, Ts, t0, tend, r1, r2)
@@ -26,7 +25,4 @@ end
 # μ  = número de pais
 # ρ  = número de pais envolvidos na procriação de uma prole
 
-result, fitness, cnt = es(custo, N;
-            initPopulation = generatePositions,
-            mutation = mutationwrapper(muts),
-μ = 15, ρ = 1, λ = P)
+result, fitness, cnt = es(custo, N; initPopulation = generatePositions, mutation = mutationwrapper(muts), μ = 15, ρ = 1, λ = P)
