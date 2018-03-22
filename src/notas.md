161a42ed8d7cfc3de331ddd2f672cb33773cc817
phelipe <phelipewesleydeoliveira@gmail.com>
2018-03-21 21:23:18 -0300

    modelo criado para otimização

---
+++ src/notas.md
@@ -0,0 +1,9 @@
+N = número de parâmetros do vetor de entrada
+λ  = descendência
+μ = número de pais
+ρ = número de pais envolvidos na procriação de uma prole
+
+result, fitness, cnt = es(nqueens, N;
+            initPopulation = generatePositions,
+            mutation = mutationwrapper(muts),
+μ = 15, ρ = 1, λ = P)
--- src/search.jl
+++ src/search.jl
@@ -9,14 +9,24 @@ r1 = 1.2 # referência junta 1
 r2 = 0.6 # referência junta 2
 
 
+function custo(x::Vector{Float64})
+
+    kp = SMatrix{2,2}(diagm([x[1], x[2]]))
+    kv = SMatrix{2,2}(diagm([x[3], x[4]]))
+    x, v, t, a, ta, j, tj = simulation(kp, kv, Ts, t0, tend, r1, r2)
+    erro1 = -(x[1] - r1)
+    erro2 = -(x[2] - r2)
+    out = sum(abs.(erro1)) + sum(abs.(erro2)) + sum(abs.(j[1])) + sum(abs.(j[2]))
+    out
+end
 
 
-# NOTE: este código ficará dentro da parte do otimizador para obter os valores de posição e jerk que serão utilizados na função objetivo.
-# NOTE: Os ganhos devem ser todos maiores ou iguais a zero
-kp = SMatrix{2,2}(diagm([1000., 30.]))
-kv = SMatrix{2,2}(diagm([250., 3.]))
-x, v, t, a, ta, j, tj = simulation(kp, kv, Ts, t0, tend, r1, r2)
-erro1 = -(x[1] - r1)
-erro2 = -(x[2] - r2)
+# N  = número de parâmetros do vetor de entrada
+# λ  = descendência
+# μ  = número de pais
+# ρ  = número de pais envolvidos na procriação de uma prole
 
-############
+result, fitness, cnt = es(custo, N;
+            initPopulation = generatePositions,
+            mutation = mutationwrapper(muts),
+μ = 15, ρ = 1, λ = P)
