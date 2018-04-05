b42313e2dc4c9dd81c69ff3d02ac5a54d888394b
Phelipe <phelipewesleydeoliveira@gmail.com>
2018-04-05 15:51:43 -0300

    editado notebook

--- src/search.jl
+++ src/search.jl
@@ -3,7 +3,7 @@ include("search-model.jl")
 using Evolutionary
 # Parâmetros da simulação
 Ts = 0.05 # Intervalo entre leituras da saída
-tend = 4.0 # tempo final para estabilização
+tend = 2.0 # tempo final para estabilização
 t0 = 0.0 # instante inicial
 r1 =  0.8#1.6# referência junta 1
 r2 =  1.6#2.0# referência junta 2
